15becbc4f815c61e022e75022750a3b72ef12ff6
Raul Kist <raulkist@gmail.com>
2025-03-12 23:28:19 -0300

    wip: logging shit

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 console.log("script log!")
 
+document.addEventListener("DOMContentLoaded", function () {
+    const importButton = document.getElementById("importButton");
+    if (importButton) {
+        importButton.addEventListener("click", logFormValues);
+    }
+});
+
 function logFormValues() {
     console.log("log values")
     const directoryName = document.getElementById('directoryName').value;
